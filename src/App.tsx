f863ca9be671e9b5e30ad5616ed58bfc4a30f120
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-10-23 11:39:45 +0200

    configured react router with some basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import { Home, About, Pricing, NotFound } from './pages';
+
 function App() {
     return (
-        <></>
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/pricing" element={<Pricing />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </>
     )
 }
 
