7dd73253e402c421964b9b874b35f734561e1037
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-10-23 14:19:40 +0200

    minor formatting fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,21 +1,27 @@
-import "../../assets/css/footer.css"
-import logo from "../../assets/img/footer-logo.png"
+import "../../assets/css/footer.css";
+import logo from "../../assets/img/footer-logo.png";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div id="container">
-        <div id="content">
-            <img src={logo} id="logo"></img>
-            <div id="about-pricing">
-                <Link to="/about" className="text">ABOUT US</Link>
-                <p className="text">|</p>
-                <Link to="/pricing" className="text">TICKETS</Link>
-            </div>
-            <p id="rights" className="text">Copyrights @Cinebh. Built with love in Sarajevo. All rights reserved.</p>
+      <div id="content">
+        <img src={logo} id="logo"></img>
+        <div id="about-pricing">
+          <Link to="/about" className="text">
+            ABOUT US
+          </Link>
+          <p className="text">|</p>
+          <Link to="/pricing" className="text">
+            TICKETS
+          </Link>
         </div>
+        <p id="rights" className="text">
+          Copyrights @Cinebh. Built with love in Sarajevo. All rights reserved.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
