f9a9e770403a5f656feafea8c0d39399401b9b7b
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-11-01 18:45:40 +0100

    extracted the box structure into a reusable component

--- src/components/Pricing/Pricing.tsx
+++ src/components/Pricing/Pricing.tsx
@@ -1,115 +1,66 @@
 import style from "./pricing.module.css";
-import Button from "../Button";
-import { FaCheck } from "react-icons/fa";
+import PriceBox from "../PriceBox.tsx/PriceBox";
 
 const Pricing = () => {
   return (
     <div id={style.container}>
       <div id={style.content}>
         <p id={style.title}>Pricing</p>
-        <p id={style.subtitle} className={style.text}>
+        <p className={style.subtitle}>
           Welcome to our cinema ticket pricing options! We offer three tiers to
           suit everyone's preferences. Explore our pricing options below and
           treat yourself to a cinematic adventure like never before!
         </p>
       </div>
       <div id={style.boxes}>
-        <div id={style.regular} className={style.box}>
-          <div className={style.first_info}>
-            <p className={style.name}>Regular Seats</p>
-            <p className={style.price}>7 KM</p>
-            <p className={style.per_ticket}>*per ticket</p>
-          </div>
-          <div className={style.second_info}>
-            <p className={style.feature1}>
-              <FaCheck className={style.check} /> Comfortable seating
-            </p>
-            <p className={style.feature2}>
-              <FaCheck className={style.check} /> Affordable pricing
-            </p>
-            <p className={style.feature3}>
-              <FaCheck className={style.check} /> Wide selection
-            </p>
-            <p className={style.feature4}>
-              <FaCheck className={style.check} /> Accessible locations
-            </p>
-            <p className={style.feature5}>
-              <FaCheck className={style.check} /> Suitable for everyone
-            </p>
-            <div className={style.button_container}>
-              <Button
-                text={"Explore movies"}
-                color={"rgba(252, 252, 253, 1)"}
-                textColor={"rgba(178, 34, 34, 1)"}
-                borderColor={"rgba(178, 34, 34, 1)"}
-              ></Button>
-            </div>
-          </div>
-        </div>
-        <div id={style.love} className={style.box}>
-          <div className={style.first_info}>
-            <p className={style.name}>Love Seats</p>
-            <p className={style.price}>24 KM</p>
-            <p className={style.per_ticket}>*per ticket</p>
-          </div>
-          <div className={style.second_info}>
-            <p className={style.feature1}>
-              <FaCheck className={style.check} /> Side-by-side design
-            </p>
-            <p className={style.feature2}>
-              <FaCheck className={style.check} /> Comfortable padding
-            </p>
-            <p className={style.feature3}>
-              <FaCheck className={style.check} /> Adjustable armrests
-            </p>
-            <p className={style.feature4}>
-              <FaCheck className={style.check} /> Cup holders
-            </p>
-            <p className={style.feature5}>
-              <FaCheck className={style.check} /> Reserved for couples
-            </p>
-            <div className={style.button_container}>
-              <Button
-                text={"Explore movies"}
-                color={"rgba(178, 34, 34, 1)"}
-                textColor={"rgba(252, 252, 253, 1)"}
-                borderColor={"rgba(178, 34, 34, 1)"}
-              ></Button>
-            </div>
-          </div>
-        </div>
-        <div id={style.vip} className={style.box}>
-          <div className={style.first_info}>
-            <p className={style.name}>Vip Seats</p>
-            <p className={style.price}>10 KM</p>
-            <p className={style.per_ticket}>*per ticket</p>
-          </div>
-          <div className={style.second_info}>
-            <p className={style.feature1}>
-              <FaCheck className={style.check} /> Enhanced comfort
-            </p>
-            <p className={style.feature2}>
-              <FaCheck className={style.check} /> Priority seating
-            </p>
-            <p className={style.feature3}>
-              <FaCheck className={style.check} /> Prime viewing
-            </p>
-            <p className={style.feature4}>
-              <FaCheck className={style.check} /> Personal space
-            </p>
-            <p className={style.feature5}>
-              <FaCheck className={style.check} /> Luxury extras
-            </p>
-            <div className={style.button_container}>
-              <Button
-                text={"Explore movies"}
-                color={"rgba(252, 252, 253, 1)"}
-                textColor={"rgba(178, 34, 34, 1)"}
-                borderColor={"rgba(178, 34, 34, 1)"}
-              ></Button>
-            </div>
-          </div>
-        </div>
+        <PriceBox
+          name="Regular Seats"
+          price="7 KM"
+          features={[
+            "Comfortable seating",
+            "Affordable pricing",
+            "Wide selection",
+            "Accessible locations",
+            "Suitable for everyone",
+          ]}
+          buttonText="Explore movies"
+          buttonColor="rgba(252, 252, 253, 1)"
+          buttonTextColor="rgba(178, 34, 34, 1)"
+          buttonBorderColor="rgba(178, 34, 34, 1)"
+          containerStyle={style.regular}
+        ></PriceBox>
+        <PriceBox
+          name="Love Seats"
+          price="24 KM"
+          features={[
+            "Side-by-side design",
+            "Comfortable padding",
+            "Adjustable armrests",
+            "Cup holders",
+            "Reserved for couples",
+          ]}
+          buttonText="Explore movies"
+          buttonColor="rgba(178, 34, 34, 1)"
+          buttonTextColor="rgba(252, 252, 253, 1)"
+          buttonBorderColor="rgba(178, 34, 34, 1)"
+          containerStyle={style.love}
+        />
+        <PriceBox
+          name="Vip Seats"
+          price="10 KM"
+          features={[
+            "Enhanced comfort",
+            "Priority seating",
+            "Prime viewing",
+            "Personal space",
+            "Luxury extras",
+          ]}
+          buttonText="Explore movies"
+          buttonColor="rgba(252, 252, 253, 1)"
+          buttonTextColor="rgba(178, 34, 34, 1)"
+          buttonBorderColor="rgba(178, 34, 34, 1)"
+          containerStyle={style.vip}
+        />
       </div>
     </div>
   );
