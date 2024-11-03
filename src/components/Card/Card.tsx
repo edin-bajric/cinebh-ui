5a8b0a913073472619182f47543968748eadc284
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-11-03 14:37:59 +0100

    fixed Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -7,43 +7,36 @@ type Props = {
 };
 
 const Card: React.FC<Props> = ({ type, data }) => {
+  const isMovie = type === "movie";
+  const isVenue = type === "venue";
+
+  const title = isMovie ? (data as Movie).title : (data as Venue).name;
+  const imageUrl = isMovie
+    ? (data as Movie).images.find((img) => img.isCoverImage)?.url
+    : (data as Venue).imageURL;
+
   return (
     <div className={style.container}>
-      {type === "movie" && (
-        <>
-          <div className={style.image_container}>
-            <img
-              className={style.image}
-              src={(data as Movie).images.find((img) => img.isCoverImage)?.url}
-              alt={(data as Movie).title}
-            />
-          </div>
-          <p className={style.title}>{(data as Movie).title}</p>
-          <div className={style.length_genre}>
-            <p className={style.length}>{(data as Movie).length} MIN</p>
-            <p className={style.divider}>|</p>
-            <p className={style.genre}>{(data as Movie).genres[0]?.name}</p>
-          </div>
-        </>
+      <div className={style.image_container}>
+        <img className={style.image} src={imageUrl} alt={title} />
+      </div>
+      <p className={style.title}>{title}</p>
+
+      {isMovie && (
+        <div className={style.length_genre}>
+          <p className={style.length}>{(data as Movie).length} MIN</p>
+          <p className={style.divider}>|</p>
+          <p className={style.genre}>{(data as Movie).genres[0]?.name}</p>
+        </div>
       )}
 
-      {type === "venue" && (
-        <>
-          <div className={style.image_container}>
-            <img
-              className={style.image}
-              src={(data as Venue).imageURL}
-              alt={(data as Venue).name}
-            />
-          </div>
-          <p className={style.title}>{(data as Venue).name}</p>
-          <div className={style.location}>
-            <p className={style.location_text}>
-              {(data as Venue).street} {(data as Venue).streetNumber},{" "}
-              {(data as Venue).city} {(data as Venue).postcode}
-            </p>
-          </div>
-        </>
+      {isVenue && (
+        <div className={style.location}>
+          <p className={style.location_text}>
+            {(data as Venue).street} {(data as Venue).streetNumber},{" "}
+            {(data as Venue).city} {(data as Venue).postcode}
+          </p>
+        </div>
       )}
     </div>
   );
