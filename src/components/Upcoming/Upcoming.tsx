bc8efdecde6cc0d1b508ae4f37f8004e22062024
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-11-09 17:31:34 +0100

    added UpcomingPage and Upcoming component, updated routing in App to include new pages

---
+++ src/components/Upcoming/Upcoming.tsx
@@ -0,0 +1,9 @@
+import style from "./upcoming.module.scss";
+
+const Upcoming = () => {
+  return (
+    <div>Upcoming</div>
+  )
+}
+
+export default Upcoming
---
+++ src/pages/UpcomingPage.tsx
@@ -0,0 +1,9 @@
+import Upcoming from "../components/Upcoming"
+
+const UpcomingPage = () => {
+  return (
+    <Upcoming />
+  )
+}
+
+export default UpcomingPage
