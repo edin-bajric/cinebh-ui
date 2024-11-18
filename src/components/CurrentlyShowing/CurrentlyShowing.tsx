435afc7027112d90cf53a5eff1647241fe5fb8e2
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-11-18 18:46:54 +0100

    refactored the code so now only the content rerenders on filter change or load more

--- src/components/CurrentlyShowing/CurrentlyShowing.tsx
+++ src/components/CurrentlyShowing/CurrentlyShowing.tsx
@@ -1,167 +1,122 @@
-import { useState, useMemo, useCallback } from "react";
+import { useMemo, useCallback, useState, useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
 import style from "./currently-showing.module.scss";
 import CurrentlyShowingAndUpcomingTitle from "../CurrentlyShowingAndUpcomingTitle";
 import Search from "../Search";
-import SelectDropdown from "../SelectDropdown";
-import DateList from "../DateList";
 import CurrentlyShowingMovieList from "./CurrentlyShowingMovieList";
-import useCurrentlyShowing from "../../hooks/useCurrentlyShowing";
 import useAllVenues from "../../hooks/useAllVenues";
 import useGenres from "../../hooks/useGenres";
 import useProjectionTimes from "../../hooks/useProjectionTimes";
-import CurrentlyShowingAndUpcomingNotFound from "../CurrentlyShowingAndUpcomingNotFound";
+import CurrentlyShowingFilters from "./CurrentlyShowingFilters";
 
 const INITIAL_PAGE_SIZE = 2;
-const PAGE_INCREMENT = 2;
 
 const CurrentlyShowing = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [totalItems, setTotalItems] = useState(0);
 
   const today = new Date().toISOString().split("T")[0];
-  const selectedTitle = searchParams.get("title") || "";
-  const selectedCity = searchParams.get("city") || "";
-  const selectedVenue = searchParams.get("cinema") || "";
-  const selectedGenre = searchParams.get("genres") || "";
-  const selectedProjectionTime = searchParams.get("projectionTime") || "";
-  const selectedDate = searchParams.get("date") || today;
 
-  const [size, setSize] = useState(
-    parseInt(searchParams.get("size") || `${INITIAL_PAGE_SIZE}`, 10)
-  );
+  const filters = {
+    title: searchParams.get("title") || "",
+    city: searchParams.get("city") || "",
+    cinema: searchParams.get("cinema") || "",
+    genres: searchParams.get("genres") || "",
+    projectionTime: searchParams.get("projectionTime") || "",
+    date: searchParams.get("date") || today,
+  };
+
+  const size = parseInt(searchParams.get("size") || `${INITIAL_PAGE_SIZE}`, 10);
 
   const { data: venuesData } = useAllVenues();
   const { data: genresData } = useGenres();
   const { data: projectionTimesData } = useProjectionTimes();
-  const { data, isLoading, error } = useCurrentlyShowing(
-    0,
-    size,
-    selectedTitle,
-    selectedCity,
-    selectedVenue,
-    selectedGenre,
-    selectedProjectionTime,
-    selectedDate
-  );
 
   const uniqueCities = useMemo(
-    () => Array.from(new Set(venuesData?.map((venue) => venue.city))),
+    () => Array.from(new Set(venuesData?.map((venue) => venue.city)) || []),
     [venuesData]
   );
 
   const uniqueProjectionTimes = useMemo(
     () =>
       Array.from(
-        new Set(projectionTimesData?.map((projection) => projection.time))
+        new Set(projectionTimesData?.map((projection) => projection.time)) || []
       ),
     [projectionTimesData]
   );
 
   const updateSearchParams = useCallback(
-    (params: Record<string, string>) => {
+    (params: Record<string, string | number>) => {
       const newParams = new URLSearchParams(searchParams);
       Object.entries(params).forEach(([key, value]) => {
-        if (value && searchParams.get(key) !== value) newParams.set(key, value);
-        else if (!value && searchParams.has(key)) newParams.delete(key);
+        if (value && `${searchParams.get(key)}` !== `${value}`) {
+          newParams.set(key, `${value}`);
+        } else if (!value && searchParams.has(key)) {
+          newParams.delete(key);
+        }
       });
       setSearchParams(newParams);
     },
     [searchParams, setSearchParams]
   );
 
-  const handleLoadMore = useCallback(() => {
-    const newSize = size + PAGE_INCREMENT;
-    setSize(newSize);
-    updateSearchParams({ size: newSize.toString() });
-  }, [size, updateSearchParams]);
-
-  const handleSearch = useCallback(
-    (e: React.FormEvent) => {
-      e.preventDefault();
-      const newTitle =
-        (e.target as HTMLFormElement).querySelector("input")?.value || "";
-      if (newTitle !== selectedTitle) {
-        setSize(INITIAL_PAGE_SIZE);
-        updateSearchParams({ title: newTitle, size: `${INITIAL_PAGE_SIZE}` });
-      }
-    },
-    [selectedTitle, updateSearchParams]
-  );
-
   const handleFilterChange = useCallback(
     (filterType: string, value: string) => {
-      if (searchParams.get(filterType) !== value) {
-        setSize(INITIAL_PAGE_SIZE);
-        updateSearchParams({
-          [filterType]: value,
-          size: `${INITIAL_PAGE_SIZE}`,
-        });
-      }
+      updateSearchParams({ [filterType]: value });
     },
-    [searchParams, updateSearchParams]
+    [updateSearchParams]
   );
 
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error loading movies.</p>;
+  const handleSizeChange = useCallback(
+    (newSize: number) => {
+      updateSearchParams({ size: newSize });
+    },
+    [updateSearchParams]
+  );
+
+  useEffect(() => {
+    if (!searchParams.has("size")) {
+      updateSearchParams({ size: INITIAL_PAGE_SIZE });
+    }
+    if (!searchParams.has("date")) {
+      updateSearchParams({ date: today });
+    }
+  }, [searchParams, updateSearchParams, today]);
 
   return (
     <div className={style.container}>
-      <div className={style.title}>
-        <CurrentlyShowingAndUpcomingTitle
-          type="currentlyShowing"
-          totalItems={data?.totalElements || 0}
-        />
-      </div>
-      <div className={style.search}>
-        <Search onSearch={handleSearch} title={selectedTitle} />
-      </div>
-      <div className={style.filters}>
-        <SelectDropdown
-          title="Cities"
-          data={uniqueCities}
-          onSelect={(value) => handleFilterChange("city", value)}
-          selectedValue={selectedCity}
-        />
-        <SelectDropdown
-          title="Venues"
-          data={venuesData?.map((venue) => venue.name) || []}
-          onSelect={(value) => handleFilterChange("cinema", value)}
-          selectedValue={selectedVenue}
-        />
-        <SelectDropdown
-          title="Genres"
-          data={genresData?.map((genre) => genre.name) || []}
-          onSelect={(value) => handleFilterChange("genres", value)}
-          selectedValue={selectedGenre}
-        />
-        <SelectDropdown
-          title="Projection Times"
-          data={uniqueProjectionTimes}
-          onSelect={(value) => handleFilterChange("projectionTime", value)}
-          selectedValue={selectedProjectionTime}
-        />
-      </div>
-      <div className={style.dates}>
-        <DateList
-          onDateSelect={(value) => handleFilterChange("date", value)}
-          selectedDate={selectedDate}
-        />
-      </div>
+      <CurrentlyShowingAndUpcomingTitle
+        type="currentlyShowing"
+        totalItems={totalItems}
+      />
+      <Search
+        onSearch={(e) => {
+          e.preventDefault();
+          const newTitle =
+            (e.target as HTMLFormElement).querySelector("input")?.value || "";
+          updateSearchParams({ title: newTitle });
+        }}
+        title={filters.title}
+      />
+      <CurrentlyShowingFilters
+        filters={filters}
+        onFilterChange={handleFilterChange}
+        venues={venuesData?.map((venue) => venue.name) || []}
+        cities={uniqueCities}
+        genres={genresData?.map((genre) => genre.name) || []}
+        projectionTimes={uniqueProjectionTimes}
+      />
       <div className={style.reminder}>
         <p>
           Quick reminder that our cinema schedule is on a ten-day update cycle.
         </p>
       </div>
-      <div className={style.showing}>
-        {data?.content.length === 0 && (
-          <CurrentlyShowingAndUpcomingNotFound type="currently-showing" />
-        )}
-        <CurrentlyShowingMovieList
-          movies={data?.content || []}
-          onLoadMore={handleLoadMore}
-          totalItems={data?.totalElements || 0}
-        />
-      </div>
+      <CurrentlyShowingMovieList
+        filters={filters}
+        size={size}
+        onSizeChange={handleSizeChange}
+        setTotalItems={setTotalItems}
+      />
     </div>
   );
 };
--- src/components/CurrentlyShowing/CurrentlyShowingMovieList/CurrentlyShowingMovieList.tsx
+++ src/components/CurrentlyShowing/CurrentlyShowingMovieList/CurrentlyShowingMovieList.tsx
@@ -1,26 +1,63 @@
+import { useCallback, useEffect } from "react";
 import style from "./currently-showing-movie-list.module.scss";
 import CurrentlyShowingCard from "../CurrentlyShowingCard";
 import { Movie } from "../../../utils/types";
 import classNames from "classnames";
+import useCurrentlyShowing from "../../../hooks/useCurrentlyShowing";
 
 type CurrentlyShowingMovieListProps = {
-  movies: Movie[];
-  onLoadMore: () => void;
-  totalItems: number;
+  filters: {
+    title: string;
+    city: string;
+    cinema: string;
+    genres: string;
+    projectionTime: string;
+    date: string;
+  };
+  size: number;
+  onSizeChange: (newSize: number) => void;
+  setTotalItems: React.Dispatch<React.SetStateAction<number>>;
 };
 
 const CurrentlyShowingMovieList: React.FC<CurrentlyShowingMovieListProps> = ({
-  movies,
-  onLoadMore,
-  totalItems,
+  filters,
+  size,
+  onSizeChange,
+  setTotalItems,
 }) => {
+  const PAGE_INCREMENT = 2;
+
+  const { data, isLoading, error } = useCurrentlyShowing(
+    0,
+    size,
+    filters.title,
+    filters.city,
+    filters.cinema,
+    filters.genres,
+    filters.projectionTime,
+    filters.date
+  );
+
+  const handleLoadMore = useCallback(() => {
+    onSizeChange(size + PAGE_INCREMENT);
+  }, [onSizeChange, size]);
+
+  const movies = data?.content || [];
+  const totalItems = data?.totalElements || 0;
   const isLoadMoreDisabled = movies.length >= totalItems;
   const isNoMovies = movies.length === 0;
+
+  useEffect(() => {
+    setTotalItems(totalItems);
+  }, [totalItems, setTotalItems]);
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error loading movies.</p>;
 
   return (
     <div className={style.container}>
       <div className={style.content}>
-        {movies.map((movie) => (
+        {movies.map((movie: Movie) => (
           <CurrentlyShowingCard key={movie.id} movie={movie} />
         ))}
         <div
@@ -28,7 +65,7 @@ const CurrentlyShowingMovieList: React.FC<CurrentlyShowingMovieListProps> = ({
             [style.disabled]: isLoadMoreDisabled,
             [style.hidden]: isNoMovies,
           })}
-          onClick={!isLoadMoreDisabled ? onLoadMore : undefined}
+          onClick={!isLoadMoreDisabled ? handleLoadMore : undefined}
         >
           <p className={style.load_more_text}>Load more</p>
         </div>
