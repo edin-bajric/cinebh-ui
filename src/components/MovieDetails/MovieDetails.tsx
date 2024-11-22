266c08fc252b81775f5fadaf94bf19039f34ccc3
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-11-22 01:50:32 +0100

    added TicketSection component and improved movieDetails

--- src/components/MovieDetails/MovieDetails.tsx
+++ src/components/MovieDetails/MovieDetails.tsx
@@ -5,6 +5,7 @@ import useMovieById from "../../hooks/useMovieById";
 import SeeAlsoList from "./SeeAlsoList";
 import Loading from "../Loading";
 import Error from "../Error";
+import TicketSection from "./TicketSection";
 
 const MovieDetails = () => {
   const { movieId } = useParams<{ movieId: string }>();
@@ -41,75 +42,77 @@ const MovieDetails = () => {
             ))}
           </div>
         </div>
-        <div className={style.info}>
-          <div className={style.title}>{data?.title}</div>
-          <div className={style.basic_info}>
-            <p className={style.rating}>{data?.rating}</p>
-            <p className={style.divider}>|</p>
-            <p className={style.language}>{data?.language}</p>
-            <p className={style.divider}>|</p>
-            <p className={style.length}>{data?.length} Min</p>
-            <p className={style.divider}>|</p>
-            <p className={style.projection_date}>
-              Projection date: {formattedStartDate} - {formattedEndDate}
-            </p>
-          </div>
-          <div className={style.genre_container}>
-            {data?.genres.map((genre) => (
-              <div key={genre.id} className={style.genre}>
-                <p>{genre.name}</p>
+        <div className={style.info_ticket_container}>
+          <div className={style.info}>
+            <div className={style.title}>{data?.title}</div>
+            <div className={style.basic_info}>
+              <p className={style.rating}>{data?.rating}</p>
+              <p className={style.divider}>|</p>
+              <p className={style.language}>{data?.language}</p>
+              <p className={style.divider}>|</p>
+              <p className={style.length}>{data?.length} Min</p>
+              <p className={style.divider}>|</p>
+              <p className={style.projection_date}>
+                Projection date: {formattedStartDate} - {formattedEndDate}
+              </p>
+            </div>
+            <div className={style.genre_container}>
+              {data?.genres.map((genre) => (
+                <div key={genre.id} className={style.genre}>
+                  <p>{genre.name}</p>
+                </div>
+              ))}
+            </div>
+            <div className={style.synopsis}>
+              <p>{data?.description}</p>
+            </div>
+            <div className={style.director}>
+              <p className={style.creator}>Director: </p>
+              <p className={style.director_name}>{data?.director}</p>
+            </div>
+            <div className={style.writers}>
+              <p className={style.creator}>Writers: </p>
+              <p className={style.writers_name}>
+                {data?.writers.map((writer) => writer.name).join(", ")}
+              </p>
+            </div>
+            <div className={style.cast}>
+              <div className={style.section_title}>
+                <p className={style.divider}>|</p>
+                <p>Cast</p>
               </div>
-            ))}
-          </div>
-          <div className={style.synopsis}>
-            <p>{data?.description}</p>
-          </div>
-          <div className={style.director}>
-            <p className={style.creator}>Director: </p>
-            <p className={style.director_name}>{data?.director}</p>
-          </div>
-          <div className={style.writers}>
-            <p className={style.creator}>Writers: </p>
-            <p className={style.writers_name}>
-              {data?.writers.map((writer) => writer.name).join(", ")}
-            </p>
-          </div>
-          <div className={style.cast}>
-            <div className={style.section_title}>
-              <p className={style.divider}>|</p>
-              <p>Cast</p>
-            </div>
-            <div className={style.cast_container}>
-              {data?.performers.map((performer) => (
-                <div key={performer.id} className={style.cast_member}>
-                  <p className={style.cast_name}>{performer.name}</p>
-                  <p className={style.cast_role}>{performer.role}</p>
-                </div>
-              ))}
-            </div>
-          </div>
-          <div className={style.movie_rating}>
-            <div className={style.section_title}>
-              <p className={style.divider}>|</p>
-              <p>Ratings</p>
-            </div>
-            <div className={style.movie_rating_container}>
-              {data?.ratings.map((rating) => (
-                <div key={rating.id} className={style.movie_rating_box}>
-                  <FaStar className={style.rating_star} />
-                  <div className={style.rating_info}>
-                    <p className={style.rating_value}>{rating.rating}</p>
-                    <p className={style.rating_provider}>{rating.name}</p>
+              <div className={style.cast_container}>
+                {data?.performers.map((performer) => (
+                  <div key={performer.id} className={style.cast_member}>
+                    <p className={style.cast_name}>{performer.name}</p>
+                    <p className={style.cast_role}>{performer.role}</p>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
+            </div>
+            <div className={style.movie_rating}>
+              <div className={style.section_title}>
+                <p className={style.divider}>|</p>
+                <p>Ratings</p>
+              </div>
+              <div className={style.movie_rating_container}>
+                {data?.ratings.map((rating) => (
+                  <div key={rating.id} className={style.movie_rating_box}>
+                    <FaStar className={style.rating_star} />
+                    <div className={style.rating_info}>
+                      <p className={style.rating_value}>{rating.rating}</p>
+                      <p className={style.rating_provider}>{rating.name}</p>
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
-          
+          <TicketSection data={data} />
         </div>
-        <div className={style.see_also_container}>
-            <SeeAlsoList movieId={data?.id}/>
-          </div>
+      </div>
+      <div className={style.see_also_container}>
+        <SeeAlsoList movieId={data?.id} />
       </div>
     </div>
   );
