0d7209193215e01f6c1e1d34ccd459675ea96b3d
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-12-02 11:20:45 +0100

    enhanced useMovieById hook with cache and refetch options for improved data handling

--- src/hooks/useMovieById.ts
+++ src/hooks/useMovieById.ts
@@ -3,9 +3,16 @@ import { MovieService } from "../services";
 import { Movie } from "../utils/types";
 
 const useMovieById = (id: string) => {
-  return useQuery<Movie>(["movie", id], async () => {
-    return await MovieService.getById(id);
-  });
+  return useQuery<Movie>(
+    ["movie", id],
+    async () => await MovieService.getById(id),
+    {
+      cacheTime: 0,
+      staleTime: 0, 
+      refetchOnMount: true, 
+      refetchOnWindowFocus: true, 
+    }
+  );
 };
 
 export default useMovieById;
