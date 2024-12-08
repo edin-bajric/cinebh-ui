6a87fbc91d6e2233921e18e7cc7ee3f01e02af56
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-12-08 15:15:37 +0100

    enhanced Navbar with active link highlighting and improved styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import style from "./navbar.module.scss";
 import Icon from "../Icon";
 import Button from "../Button";
@@ -14,13 +14,14 @@ const Navbar = () => {
   const [modalType, setModalType] = useState<"signin" | "signup">("signin");
   const { userToken } = useSelector((state: RootState) => state.auth);
   const [decodedToken, setDecodedToken] = useState<any>(null);
+  const location = useLocation();
 
   const toggleModal = () => {
     setModalOpen((prev) => !prev);
   };
 
   const closeAllModals = () => {
-    setModalOpen(false); 
+    setModalOpen(false);
     setModalType("signin");
   };
 
@@ -42,18 +43,29 @@ const Navbar = () => {
 
   const username = decodedToken?.sub.split("@")[0];
 
+  const isActive = (path: string) => location.pathname === path;
+
   return (
     <>
       <div className={`${style.container} ${isModalOpen ? style.blur : ""}`}>
         <Icon />
         <div className={style.nav}>
-          <Link to="/currently-showing" className={style.link}>
+          <Link
+            to="/currently-showing"
+            className={`${style.link} ${isActive("/currently-showing") ? style.active : ""}`}
+          >
             Currently Showing
           </Link>
-          <Link to="/upcoming" className={style.link}>
+          <Link
+            to="/upcoming"
+            className={`${style.link} ${isActive("/upcoming") ? style.active : ""}`}
+          >
             Upcoming Movies
           </Link>
-          <Link to="/venues" className={style.link}>
+          <Link
+            to="/venues"
+            className={`${style.link} ${isActive("/venues") ? style.active : ""}`}
+          >
             Venues
           </Link>
         </div>
