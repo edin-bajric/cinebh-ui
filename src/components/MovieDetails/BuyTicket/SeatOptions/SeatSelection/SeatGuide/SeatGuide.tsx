44dc5920b8b23d88e4ed33f633869b3bf9ef93ec
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-12-09 12:20:41 +0100

    added BuyTicket component with basic structure and also SeatOptions Tab with basic structure

---
+++ src/components/MovieDetails/BuyTicket/SeatOptions/SeatSelection/SeatGuide/SeatGuide.tsx
@@ -0,0 +1,9 @@
+import style from "./seat-guide.module.scss";
+
+const SeatGuide = () => {
+  return (
+    <div>SeatGuide</div>
+  )
+}
+
+export default SeatGuide
---
+++ src/components/MovieDetails/BuyTicket/SeatOptions/SeatSelection/SeatSelection.tsx
@@ -0,0 +1,10 @@
+import style from "./seat-selection.module.scss";
+import SeatGuide from "./SeatGuide";
+
+const SeatSelection = () => {
+  return (
+    <div>SeatSelection</div>
+  )
+}
+
+export default SeatSelection
