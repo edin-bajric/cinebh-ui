59c1de5516fd34decba9f6e1b83fea94f5ee1fac
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2024-12-12 00:41:01 +0100

    added ChoosenSeats component and integrated it into SeatSelection with updated styling and layout

--- src/components/MovieDetails/BuyTicket/SeatOptions/SeatSelection/SeatSelection.tsx
+++ src/components/MovieDetails/BuyTicket/SeatOptions/SeatSelection/SeatSelection.tsx
@@ -1,16 +1,20 @@
 import style from "./seat-selection.module.scss";
 import SeatGuide from "./SeatGuide";
 import SeatArrangement from "./SeatArrangement";
-
+import ChoosenSeats from "./ChoosenSeats";
+import Button from "../../../../Button";
 const SeatSelection = () => {
   return (
     <div className={style.container}>
       <div className={style.arrangement}>
-      <SeatArrangement />
+        <SeatArrangement />
       </div>
-      
-      <div className={style.guide_choosen}>
+      <div className={style.guide_choosen_button}>
+        <div className={style.guide_choosen}>
         <SeatGuide />
+        <ChoosenSeats />
+        </div>
+        <Button text="Continue to Payment" />
       </div>
     </div>
   );
