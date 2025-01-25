993584b5fc366ce6a616ea0c0fb0b14ca7f87b4d
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2025-01-25 02:13:57 +0100

    implemented payment form with validation and input formatting; added reusable FormInput component

--- src/components/MovieDetails/BuyTicket/PaymentDetails/NewCard/NewCard.tsx
+++ src/components/MovieDetails/BuyTicket/PaymentDetails/NewCard/NewCard.tsx
@@ -1,30 +1,67 @@
 import style from "./new-card.module.scss";
 import Button from "../../../../Button";
+import { FaCreditCard } from "react-icons/fa6";
+import { useState } from "react";
+import {
+  handleCardNumberChange,
+  handleExpiryDateChange,
+  handleCvvChange,
+} from "./newCardUtils";
+import { useLocation } from "react-router-dom";
+import FormInput from "./FormInput";
 
 const NewCard = () => {
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+
+  const location = useLocation();
+  const { state } = location;
+  const { totalPrice } = state || {};
+
+  const isFormValid =
+    cardNumber.length === 19 && expiryDate.length === 5 && cvv.length === 3;
+
   return (
     <div className={style.container}>
       <form className={style.card_form}>
-        <div className={style.form_group}>
-          <label htmlFor="card_number">Card Number</label>
-          <input type="text" id="card_number" placeholder="Enter card number" />
-        </div>
+        <FormInput
+          label="Card Number"
+          id="card_number"
+          type="text"
+          value={cardNumber}
+          onChange={(e) => handleCardNumberChange(e, setCardNumber)}
+          placeholder="**** **** **** ****"
+          maxLength={19}
+          icon={FaCreditCard}
+        />
         <div className={style.expiry_cvv}>
-          <div className={style.form_group}>
-            <label htmlFor="expiry_date">Expiry Date</label>
-            <input type="text" id="expiry_date" placeholder="MM/YY" />
-          </div>
-          <div className={style.form_group}>
-            <label htmlFor="cvv">CVV</label>
-            <input type="text" id="cvv" placeholder="Enter CVV" />
-          </div>
+          <FormInput
+            label="Expiry Date"
+            id="expiry_date"
+            type="text"
+            value={expiryDate}
+            onChange={(e) => handleExpiryDateChange(e, setExpiryDate)}
+            placeholder="MM/YY"
+            maxLength={5}
+          />
+          <FormInput
+            label="CVV"
+            id="cvv"
+            type="text"
+            value={cvv}
+            onChange={(e) => handleCvvChange(e, setCvv)}
+            placeholder="000"
+            maxLength={3}
+          />
         </div>
         <Button
-          text="Make Payment"
+          text={`Make Payment - ${totalPrice ? `${totalPrice} KM` : ""}`}
           type="submit"
           variant="solid"
           width="100%"
-        ></Button>
+          disabled={!isFormValid}
+        />
       </form>
     </div>
   );
---
+++ src/components/MovieDetails/BuyTicket/PaymentDetails/NewCard/newCardUtils.ts
@@ -0,0 +1,30 @@
+export const handleCardNumberChange = (
+  e: React.ChangeEvent<HTMLInputElement>,
+  setCardNumber: React.Dispatch<React.SetStateAction<string>>
+) => {
+  const inputValue = e.target.value.replace(/\s+/g, "");
+  if (/^\d*$/.test(inputValue)) {
+    const formattedValue = inputValue.replace(/(\d{4})(?=\d)/g, "$1 ");
+    setCardNumber(formattedValue);
+  }
+};
+
+export const handleExpiryDateChange = (
+  e: React.ChangeEvent<HTMLInputElement>,
+  setExpiryDate: React.Dispatch<React.SetStateAction<string>>
+) => {
+  let inputValue = e.target.value.replace(/\D/g, "");
+  if (inputValue.length > 4) inputValue = inputValue.slice(0, 4);
+  if (inputValue.length > 2) {
+    inputValue = inputValue.slice(0, 2) + "/" + inputValue.slice(2);
+  }
+  setExpiryDate(inputValue);
+};
+
+export const handleCvvChange = (
+  e: React.ChangeEvent<HTMLInputElement>,
+  setCvv: React.Dispatch<React.SetStateAction<string>>
+) => {
+  const inputValue = e.target.value.replace(/\D/g, "");
+  if (inputValue.length <= 3) setCvv(inputValue);
+};
