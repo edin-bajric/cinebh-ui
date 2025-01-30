3d66288bc6bb594485cf69d4f07f864ecdef2650
edin-bajric <edin.bajric@stu.ibu.edu.ba>
2025-01-30 21:24:06 +0100

    added basic SavedCard implementation

--- src/components/MovieDetails/BuyTicket/PaymentDetails/SavedCards/SavedCard/SavedCard.tsx
+++ src/components/MovieDetails/BuyTicket/PaymentDetails/SavedCards/SavedCard/SavedCard.tsx
@@ -1,8 +1,21 @@
 import style from "./saved-card.module.scss";
+import { FaCcVisa } from "react-icons/fa6";
 
 const SavedCard = () => {
   return (
-    <div>SavedCard</div>
+    <div className={style.container}>
+      <div className={style.content}>
+          <div className={style.icon}>
+            <FaCcVisa />
+          </div>
+          <div className={style.card_number}>
+            <p>**** **** **** 1234</p>
+          </div>
+          <div className={style.delete_card}>
+            <p>Delete Card</p>
+          </div>
+      </div>
+    </div>
   )
 }
 
